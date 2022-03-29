e184b684550dcfb9a38f79faf546ccbd6140be81
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-03-30 03:04:48 +0900

    2022.03.29 관계연산자

--- src/main/java/Ex04_05.java
+++ src/main/java/Ex04_05.java
@@ -9,7 +9,7 @@ public class Ex04_05 {
         System.out.printf("%d >= %d 는 %s이다.\n", a, b, a >= b);
         System.out.printf("%d <= %d 는 %s이다.\n", a, b, a <= b);
 
-        System.out.printf("%d = %d 는 %s이다.\n", a, b, a = b);
+        System.out.printf("%d = %d 는 %s이다.\n", a, b, a == b);
 
 
 
